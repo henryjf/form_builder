a689df1b9462a4effbf665bacf9df299218a2324
JEFF HENRY <henry_jf@hotmail.com>
2016-03-15 16:00:35 -0400

    attempting to make js work

--- src/js/index.js
+++ src/js/index.js
@@ -8,19 +8,25 @@ import { applicant, phone, language, email, comment} from './template';
 
 var $app = $('.app');
 
-users.forEach(function(user) {
-	var template;
-	if (user.type === 'text') {
-		template = applicant;
-	} else if (user.type==='tel') {
-		template = phone;
-	} else if (user.type==='email') {
-		template = email;
-	} else if (user.type==='select') {
-		template = language;
-	} else if (user.type==='textarea') {
-		template = comment;
-	}
+// users.forEach(function(user) {
+// 	var template;
+// 	if (user.type === 'text') {
+// 		template = applicant;
+// 	} else if (user.type==='tel') {
+// 		template = phone;
+// 	} else if (user.type==='email') {
+// 		template = email;
+// 	} else if (user.type==='select') {
+// 		template = language;
+// 	} else if (user.type==='textarea') {
+// 		template = comment;
+// 	}
+// 	var html = template(user);
+// 	$app.append(html);
+// })
+
+users.forEach(function(user){
+	var template = userTypeToTemplate[user.type];
 	var html = template(user);
 	$app.append(html);
 })
