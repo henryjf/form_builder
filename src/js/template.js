45758505b0bb846e6eb06c546384ef5a4a9606ff
JEFF HENRY <henry_jf@hotmail.com>
2016-03-14 22:32:48 -0400

    removed commented out code

--- src/js/template.js
+++ src/js/template.js
@@ -10,9 +10,6 @@ return `
   	`;
 
 
-  	// <input type="text" name="email" placeholder="Email address"><br>
-  	// <input type="text" name="website" placeholder="Current website url"><br>
-  	// <input type="text" name="language" value="Select language...">
 export function phone(user) {
 
 	var lab = user.label;
@@ -56,7 +53,7 @@ export function language(user) {
 
   		`;
 	}
-	
+
 
 export function email(user) {
 	return `
