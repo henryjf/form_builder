4dc52231809ada1192de6afb7c13229c475df8c5
JEFF HENRY <henry_jf@hotmail.com>
2016-03-14 22:30:51 -0400

    added index.js and scss

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,21 @@ import $ from 'jquery';
 
 import users from './users';
 
-// import { applicant, phone } from './template';
+import { applicant, phone, language, email, comment} from './template';
 
 var $app = $('.app');
+
+users.forEach(function(user) {
+	var template;
+	if (user.type === 'text') {
+		template = applicant;
+	} else if (user.type==='tel') {
+		template = phone;
+	} else if (user.type==='email') {
+		template = email;
+	} else if (user.type==='select') {
+		template = language;
+	} else if (user.type==='textarea') {
+		template = comment;
+	}
+})
--- src/js/template.js
+++ src/js/template.js
@@ -6,8 +6,9 @@ export function applicant(user) {
 return `
 	<input type="text" name="firstname">${user.label}<br>
   	<input type="text" name="lastname">${user.label}<br>
-  	<input type="text" name="comment">${user.label}<br>
+  	<input type="text" name="website">${user.label}<br>
   	`;
+
 
   	// <input type="text" name="email" placeholder="Email address"><br>
   	// <input type="text" name="website" placeholder="Current website url"><br>
@@ -16,8 +17,55 @@ export function phone(user) {
 
 	var lab = user.label;
 	return `${lab}`;
-}
-return`
-	<input type="text" name="mobile">${user.label}<br>
-  	<input type="text" name="home" placeholder="Home number"><br>
+	}
+	return`
+		<input type="tel" name="mobile">${user.label}<br>
+	  	<input type="tel" name="home">${user.label}<br>
 	`;
+
+
+export function comment(user) {
+	var lab = user.label;
+	return `${lab}`;
+	}
+	return`
+		<input type="textarea" name="comment">${user.label}<br>
+	`;
+
+
+export function language(user) {
+
+	var lang = user.label;
+
+	var langHtml = lang.map(function(lan) {
+		return `<option>${lan}</option>`;
+	});
+
+	langHtml = langHtml.join('');
+
+	return `
+	<input type="text" name="language">${user.label}<br>
+  		<select>	
+  			<option value="EN">English</option>
+  			<option value="FR">French</option>
+  			<option value="SP">Spanish</option>
+  			<option value="CH">Chinese</option>
+  			<option value="JP">Japanese</option>
+  			
+  		</select><br>
+
+  		`;
+	}
+	
+
+export function email(user) {
+	return `
+	<input type="email" name="email">${user.label}<br>
+	`;
+	}
+
+
+
+
+
+
